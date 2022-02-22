935e0de7ba9ddd64377f07723b8d25950aa9a83d
Ankit Kumar <ankitgiri190@gmail.com>
2022-02-22 15:00:23 +0530

    filter added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -127,47 +127,8 @@ setTimeout(function () {
     })(window.jQuery);
 }, 1000);
 
-// table search function
-// Light Javascript Table Filter
-// -by Chris Coyier
-(function (document) {
-  "use strict";
-  var LightTableFilter = (function (Arr) {
-    var _input;
-    function _onInputEvent(e) {
-      _input = e.target;
-      var tables = document.getElementsByClassName(
-        _input.getAttribute("data-table")
-      );
-      Arr.forEach.call(tables, function (table) {
-        Arr.forEach.call(table.tBodies, function (tbody) {
-          Arr.forEach.call(tbody.rows, _filter);
-        });
-      });
-    }
-    function _filter(row) {
-      var text = row.textContent.toLowerCase(),
-        val = _input.value.toLowerCase();
-      row.style.display = text.indexOf(val) === -1 ? "none" : "table-row";
-    }
-    return {
-      init: function () {
-        var inputs = document.getElementsByClassName("table-filter");
-        Arr.forEach.call(inputs, function (input) {
-          input.oninput = _onInputEvent;
-        });
-      },
-    };
-  })(Array.prototype);
+// table search filter section start
 
-  document.addEventListener("readystatechange", function () {
-    if (document.readyState === "complete") {
-      LightTableFilter.init();
-    }
-  });
-})(document); // table search function
-// Light Javascript Table Filter
-// -by Chris Coyier
 (function (document) {
   "use strict";
   var LightTableFilter = (function (Arr) {
@@ -204,3 +165,41 @@ setTimeout(function () {
     }
   });
 })(document);
+(function (document) {
+  "use strict";
+  var LightTableFilter = (function (Arr) {
+    var _input;
+    function _onInputEvent(e) {
+      _input = e.target;
+      var tables = document.getElementsByClassName(
+        _input.getAttribute("data-table")
+      );
+      Arr.forEach.call(tables, function (table) {
+        Arr.forEach.call(table.tBodies, function (tbody) {
+          Arr.forEach.call(tbody.rows, _filter);
+        });
+      });
+    }
+    function _filter(row) {
+      var text = row.textContent.toLowerCase(),
+        val = _input.value.toLowerCase();
+      row.style.display = text.indexOf(val) === -1 ? "none" : "table-row";
+    }
+    return {
+      init: function () {
+        var inputs = document.getElementsByClassName("table-filter");
+        Arr.forEach.call(inputs, function (input) {
+          input.oninput = _onInputEvent;
+        });
+      },
+    };
+  })(Array.prototype);
+
+  document.addEventListener("readystatechange", function () {
+    if (document.readyState === "complete") {
+      LightTableFilter.init();
+    }
+  });
+})(document);
+
+// table search filter ended
