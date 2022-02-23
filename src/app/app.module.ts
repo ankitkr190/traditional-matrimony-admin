29087b4fec2085099c6fd924b875cc36d61e47d5
Ankit Kumar <ankitgiri190@gmail.com>
2022-02-23 16:52:58 +0530

    fixed bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LocationComponent } from './location/location.component';
 import { LoginComponent } from './login/login.component';
 import { ProfessionComponent } from './profession/profession.component';
-<<<<<<< HEAD
+
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-=======
+
 import { CasteComponent } from './caste/caste.component';
 import { EducationComponent } from './education/education.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
@@ -18,7 +18,7 @@ import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 import { VerifiedComponent } from './verified/verified.component';
 import { UnverifiedComponent } from './unverified/unverified.component';
 
->>>>>>> 3ccbc9212e21360f8dc8989fd6aefdbeb48516d5
+
 
 @NgModule({
   declarations: [
